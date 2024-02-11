4fbacbcfd4d464eb19737410053b256ea987d409
zeyn_app <125817782+zeynnep@users.noreply.github.com>
2024-02-12 01:56:39 +0300

    5.R

--- 5.R
+++ 5.R
@@ -2,5 +2,12 @@
 
 # "rm()" silmek istediğiniz değeri rm nin içiine yazdığınızda listeden çıkartabilirsiniz.
  
- # mode() bu komut içine yazdığımız değerin hangi veri türünde olduğunu söyyler.
+ # mode() bu komut içine yazdığımız değerin hangi veri türünde olduğunu söyler.
+
+# Rank() sıralanan vektördeki değerin konumuna referans verir ve orijinal sıra ile aynıdır. Temelde küçükten büyüğe sıralama hedeflidir, eksi işareti ile büyükten küçüğe sıralama şekline geçilebilir.
  
+# Order, orijinal değerin konumunu belirler ve sıralar. Temelde küçükten büyüğe sıralama hedeflidir, eksi işareti ile büyükten küçüğe sıralama şekline geçilebilir. Özellikle indeksleme işlemi için kullanışlı bir fonksiyodur.
+
+# Bir vektörü elemanlarının artan veya azalan sırasına göre sıralamak, sıklıkla ihtiyaç duyulan işlevlerden biridir. Bu amaçla kullanılan fonksiyon sort() olmakla birlikte decreasing seçimine bağlı olarak artan veya azalan şekilde sıralama yapmak mümkündür.
+
+# Bir vektörü elemanlarının artan veya azalan sırasına göre sıralamak, sıklıkla ihtiyaç duyulan işlevlerden biridir. Bu amaçla kullanılan fonksiyon sort() olmakla birlikte decreasing seçimine bağlı olarak artan veya azalan şekilde sıralama yapmak mümkündür.
